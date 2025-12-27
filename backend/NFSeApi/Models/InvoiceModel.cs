c5fa985663fc955df1af712d97713e72ca3aff7a
Otávio Daniel <dotavioaug@gmail.com>
2025-12-27 16:33:18 -0300

    variable changes

--- backend/NFSeApi/Models/InvoiceModel.cs
+++ backend/NFSeApi/Models/InvoiceModel.cs
@@ -6,8 +6,9 @@ public class InvoiceModels
 	public string NFSeNumber { get; set; }
 	public string CompanyName { get; set; }
 	public string CNPJ { get; set; }
-	public string IssueDate { get; set; }
-	public double Price { get; set; }
+	public DateTime IssueDate { get; set; }
+	public DateTime CreatedAt { get; set; }
+	public Decimal Price { get; set; }
 	public string Validation { get; set; }
 
 }
